4843b0423d5c18ccfd58db74d98b4e0375541a7e
Rob Garner <rgarner011235@gmail.com>
2024-04-09 19:16:14 -0600

    Update BalloonController.cs

--- Assets/Scripts/BalloonScripts/BalloonController.cs
+++ Assets/Scripts/BalloonScripts/BalloonController.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class BalloonController : MonoBehaviour
 {
-    public GameObject balloon;
-    public float balloonDensity = 1.204f; //kg/m^3 start same density as air
+    public float windDirectionChangeAltitude = 200.0f;
+    public float windForce = 20.0f;
+    public Vector3 groundWindDirection = new Vector3(1, 0, 0);
+    private ConstantForce force;
 
+    public float balloonDensity = 1.204f; //kg/m^3 start same density as air
 
     public Buoyancy buoyancy;
 
     // Start is called before the first frame update
     void Start()
     {
+        force = GetComponent<ConstantForce>();
         buoyancy = new Buoyancy();
         buoyancy.ObjectDensity = balloonDensity; //kg/m^3
         buoyancy.ObjectVolume = 0.5f; //m^3
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        var yForce = buoyancy.BuoyancyForce;
         
+        force.force = new Vector3(0, yForce, 0);
+    }
+
+    private float CalculateWindForce(float altitude)
+    {
+        return windForce * (1 - altitude / windDirectionChangeAltitude);
     }
 }
