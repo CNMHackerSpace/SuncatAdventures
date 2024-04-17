2d0f0c7165e803a2dbf14e764fb997794e45f34a
lrice92 <107086207+lrice92@users.noreply.github.com>
2024-04-16 21:10:35 -0600

    Added Controller and Settings popup scripts

---
+++ Assets/Scripts/BalloonPopScripts/SettingsPopup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SettingsPopup : MonoBehaviour
+{
+
+    public void Open()
+    {
+        this.gameObject.SetActive(true);
+    }
+    public void Close()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
---
+++ Assets/Scripts/BalloonPopScripts/UIController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text balloonsRemaining;
+    [SerializeField] private SettingsPopup settingsPopup;
+    [SerializeField] private Text dartCounter;
+    [SerializeField] private CountdownTimer countdownTimer;
+    private int _score;
+
+    void Awake()
+    {
+        //Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void OnDestroy()
+    {
+        //Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    public void OnOpenSettings()
+    {
+        settingsPopup.Open();
+    }
+    public void OnPointerDown()
+    {
+        Debug.Log("pointer down");
+    }
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        _score = 0;
+
+        settingsPopup.Close();
+       
+    }
+
+    // Update is called once per frame
+    private void OnEnemyHit()
+    {
+        _score += 1;
+        balloonsRemaining.text = _score.ToString();
+    }
+}
