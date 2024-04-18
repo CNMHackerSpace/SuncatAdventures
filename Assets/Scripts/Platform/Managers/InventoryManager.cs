56918eee7f430696f633140af3dc1e73ca941858
Patrick block <143564507+PtrickBlock@users.noreply.github.com>
2024-04-18 13:01:56 -0600

    Patrick Block

---
+++ Assets/Scripts/Platform/Managers/InventoryManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Collections;
+
+
+
+public class InventoryManager : MonoBehaviour, IGameManager
+{
+
+    private Dictionary<string, int> items;
+    public ManagerStatus status { get; private set; }
+
+    public string equippedItem { get; private set; }
+
+    public void Startup()
+    {
+        Debug.Log("Inventory manager starting...");
+        items = new Dictionary<string, int>();
+        status = ManagerStatus.Started;
+    }
+
+    private void DisplayItems()
+    {
+        string itemDisplay = "Items: ";
+        foreach (KeyValuePair<string, int> item in items)
+        {
+            itemDisplay += $"{item.Key} ({item.Value})";
+        }
+        Debug.Log(itemDisplay);
+    }
+    public void AddItem(string name)
+    {
+        if (items.ContainsKey(name))
+        {
+            items[name] += 1;
+        }
+        else
+        {
+            items[name] = 1;
+        }
+
+        DisplayItems();
+    }
+    public List<string> GetItemList()
+    {
+        List<string> list = new List<string>(items.Keys);
+        return list;
+    }
+
+    public int GetItemCount(string name)
+    {
+        int number = 0;
+        if (items.ContainsKey(name))
+        {
+            number = items[name];
+        }
+        return number;
+    }
+
+    public bool EquipItem(string name)
+    {
+        if (items.ContainsKey(name) && equippedItem != name)
+        {
+            equippedItem = name;
+            Debug.Log($"Equipped {name}");
+            return true;
+        }
+
+        equippedItem = null;
+        Debug.Log("Unequipped");
+        return false;
+    }
+}
