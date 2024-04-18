62f797d9b3761e823097e6a57a01529c93b3f6d8
Josh Bivens <thejoshbivens@gmail.com>
2024-04-18 15:12:31 -0600

    Add textures

--- Assets/Scripts/SnapArea.cs
+++ Assets/Scripts/SnapArea.cs
@@ -7,20 +7,18 @@ public class SnapArea : MonoBehaviour
     // When a reactivetarget of type motherboard enters the snap area, it should snap to the area
     [SerializeField] string type;
     ReactiveTarget target;
+    bool snapped = false;
 
     private void OnTriggerEnter(Collider other)
     {
         target = other.GetComponent<ReactiveTarget>();
-        if (target != null && this.type == target.Type)
+        if (target != null && this.type == target.Type && !snapped)
         {
             // Snap the object to the area
             target.gameObject.transform.SetLocalPositionAndRotation(transform.position, transform.rotation);
+            snapped = true;
         }
     }
-
-    // Get type
-    public string Type
-    {
-        get { return type; }
-    }
+    
+    public string Type { get => type;}
 }
