9c67528cdd01a7456fa2ef6a13cb39f6e2edb098
FerrellS <fsanchezharo@cnm.edu>
2024-04-18 22:48:14 -0600

    Score

--- Assets/Scripts/FSScripts/Score.cs
+++ Assets/Scripts/FSScripts/Score.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
 
     [SerializeField] int score;
+    [SerializeField] TMP_Text scoreLabel;
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        scoreLabel.text = score.ToString();
+    }
 
     public int GetScore()
     {
